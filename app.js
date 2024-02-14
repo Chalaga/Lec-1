219c33763f2a1d6a1fa8e2106d2932b0c53fba69
Chalaga <150872963+Chalaga@users.noreply.github.com>
2024-02-14 21:56:25 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,62 @@
+// 1) დაწერეთ კოდი სადაც შეამოწმებთ დადებითია თუ უარყოფითი
+// ეს რიცხვი!!
+
+//asnwer:
+
+// if (num > 0) {
+//     console.log("დადებითია");
+// } else {
+//     console.log("უარყოფითია");
+// }
+
+//2)დაწერეთ მარტივი კალკულატორი სადაც გამოიყენებთ ოპერატორებს(+, -, *, /)
+// switch case დახმარებით უბრალოდ დარწმუნდით რომ თითოეულ ქეისი ასრულებს ამ მეთოდებს
+
+//asnwer:
+
+// let calculate = '='
+
+// switch (calculate) {
+//     case '+':
+//         console.log(10 + 10);
+//         break;
+//     case '-':
+//         console.log(10 - 10);
+//         break;
+//     case '*':
+//         console.log(10 * 10);
+//         break;
+//     case '/':
+//         console.log(10 / 10);
+//         break;
+//     default:
+//         console.log('Calculation is wrong');
+//         break;
+// }
+
+//3) დაწერეთ იგივე კალკულატორი if else დახმარებით!!
+
+// let key = "+";
+
+// if (key == "+") {
+//   console.log(10 + 10);
+// } else if (key == "-") {
+//   console.log(10 - 10);
+// } else if (key == "*") {
+//   console.log(10 * 10);
+// } else if (key == "/") {
+//   console.log(10 / 10);
+// } else {
+//   console.log("Calculation is wrong");
+// }
+
+//asnwer:
+
+//4) დაწერეთ ავტორიზაციის ლოგიკა სადაც უნდა დავარეგისტრიროთ User
+// user უნდა შემოყავდეს firstName,lastName,email,password,phoneNumber
+// firstName,lastName,email,password არის სავალდებულო ველები
+// ამიტომ ერთიანად შეამოწმეთ ეს ოთხი ველი და დარწმუნდით თუ ცარიელი input
+// არარის მაშინ დალოგეთ რომ შენ დარეგისტრირდი წარმატებით თუ ცარიელია მაშინ
+// დალოგეთ "input is required" გამოიყენეთ if else
+
+//asnwer:
